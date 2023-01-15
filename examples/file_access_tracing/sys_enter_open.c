790050d833cbf8b97694e37d4f23f3d9dec21f3a
qinghen.lc <qinghen.lc@alibaba-inc.com>
2023-01-15 16:45:25 +0800

    m

--- examples/file_access_tracing/sys_enter_open.c
+++ examples/file_access_tracing/sys_enter_open.c
@@ -5,16 +5,11 @@
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
-#define TASK_COMM_LEN 16
-
 struct event {
   u32 syscall_nr;
   u8 filename[400];
   u32 flags;
   u32 mode;
-  int pid;
-  char comm[TASK_COMM_LEN];
-  int ppid;
 };
 
 struct {
@@ -45,22 +40,6 @@ struct syscalls_enter_openat_args {
   long long mode;
 };
 
-/*int endWith(char *originString, char *end) {
-  if (originString == NULL || end == NULL ||
-      strlen(end) > strlen(originString)) {
-    return -1;
-  }
-  int n = strlen(end);
-  int m = strlen(originString);
-  int i;
-  for (i = 0; i < n; i++) {
-    if (originString[m - i - 1] != end[n - i - 1]) {
-      return 1;
-    }
-  }
-  return 0;
-}*/
-
 SEC("tracepoint/syscalls/sys_enter_openat")
 int sys_enter_open(struct syscalls_enter_openat_args *ctx) {
   char *fname = (char *)(ctx->filename_ptr);
@@ -70,12 +49,6 @@ int sys_enter_open(struct syscalls_enter_openat_args *ctx) {
   event.mode = ctx->mode;
   bpf_probe_read_str(&event.filename, sizeof(event.filename), fname);
 
-  event.pid = bpf_get_current_pid_tgid() >> 32;
-  bpf_get_current_comm(&event.comm, TASK_COMM_LEN);
-  struct task_struct *task = (struct task_struct *)bpf_get_current_task();
-  if (task != NULL) {
-    event.ppid = task->real_parent->tgid
-  }
 
   bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
   return 0;
