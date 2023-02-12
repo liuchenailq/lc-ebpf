19fc78bc4038345ae1678c10e5d8a8dfabe5749c
liuchenailq <2422701543@qq.com>
2023-02-12 16:53:10 +0800

    a

--- examples/cpu_idle/trace_idle_with_hash_map.c
+++ examples/cpu_idle/trace_idle_with_hash_map.c
@@ -8,18 +8,18 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 
 // start idle time of per-cpu
 struct {
-	__uint(type, BPF_MAP_TYPE_ARRAY);
-	__uint(max_entries, MAX_MAP_ENTRIES);
-	__type(key, __u32);
-	__type(value, __u64);
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __uint(max_entries, MAX_MAP_ENTRIES);
+  __type(key, __u32);
+  __type(value, __u64);
 } start_idle_time_map SEC(".maps");
 
 // idle duration time of per-cpu
 struct {
-	__uint(type, BPF_MAP_TYPE_ARRAY);
-	__uint(max_entries, MAX_MAP_ENTRIES);
-	__type(key, __u32);
-	__type(value, __u64);
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __uint(max_entries, MAX_MAP_ENTRIES);
+  __type(key, __u32);
+  __type(value, __u64);
 } idle_duration_time_map SEC(".maps");
 
 SEC("kprobe/do_idle")
@@ -28,10 +28,10 @@ int kprobe_do_idle(struct pt_regs *ctx) {
   __u32 cpu = bpf_get_smp_processor_id();
   __u64 now = bpf_ktime_get_ns();
   __u64 *start_idle_time = bpf_map_lookup_elem(&start_idle_time_map, &cpu);
-  if(start_idle_time){
-        bpf_map_update_elem(&start_idle_time_map, &cpu, &now, BPF_EXIST);
-  }else{
-        bpf_map_update_elem(&start_idle_time_map, &cpu, &now, BPF_NOEXIST);
+  if (start_idle_time) {
+    bpf_map_update_elem(&start_idle_time_map, &cpu, &now, BPF_EXIST);
+  } else {
+    bpf_map_update_elem(&start_idle_time_map, &cpu, &now, BPF_NOEXIST);
   }
   return 0;
 }
@@ -42,15 +42,17 @@ int kprobe_schedule_idle(struct pt_regs *ctx) {
   __u32 cpu = bpf_get_smp_processor_id();
   __u64 now = bpf_ktime_get_ns();
   __u64 *start_idle_time = bpf_map_lookup_elem(&start_idle_time_map, &cpu);
-  if(start_idle_time){
-        __u64 duration_time = now - *start_idle_time;
-        __u64 *value = bpf_map_lookup_elem(&idle_duration_time_map, &cpu);
-        if(value){
-            duration_time = duration_time + *value;
-            bpf_map_update_elem(&idle_duration_time_map, &cpu, &duration_time, BPF_NOEXIST);
-        }else{
-            bpf_map_update_elem(&idle_duration_time_map, &cpu, &duration_time, BPF_NOEXIST);
-        }
+  if (start_idle_time) {
+    __u64 duration_time = now - *start_idle_time;
+    __u64 *value = bpf_map_lookup_elem(&idle_duration_time_map, &cpu);
+    if (value) {
+      duration_time = duration_time + *value;
+      bpf_map_update_elem(&idle_duration_time_map, &cpu, &duration_time,
+                          BPF_NOEXIST);
+    } else {
+      bpf_map_update_elem(&idle_duration_time_map, &cpu, &duration_time,
+                          BPF_NOEXIST);
+    }
   }
   return 0;
 }
