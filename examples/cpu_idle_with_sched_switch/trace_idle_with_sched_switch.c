2d9d6eaaad8afadc88a15b18dfffc4c7824fb96f
qinghen.lc <qinghen.lc@alibaba-inc.com>
2023-05-31 20:57:59 +0800

    m

--- examples/cpu_idle_with_sched_switch/trace_idle_with_sched_switch.c
+++ examples/cpu_idle_with_sched_switch/trace_idle_with_sched_switch.c
@@ -6,35 +6,23 @@
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
-// #define MAX_MAP_ENTRIES 20
-//
-//// start idle time of per-cpu
-// struct {
-//   __uint(type, BPF_MAP_TYPE_ARRAY);
-//   __uint(max_entries, MAX_MAP_ENTRIES);
-//   __type(key, __u32);
-//   __type(value, __u64);
-// } start_idle_time_map SEC(".maps");
-//
-//// idle duration time of per-cpu
-// struct {
-//   __uint(type, BPF_MAP_TYPE_ARRAY);
-//   __uint(max_entries, MAX_MAP_ENTRIES);
-//   __type(key, __u32);
-//   __type(value, __u64);
-// } idle_duration_time_map SEC(".maps");
+ #define MAX_MAP_ENTRIES 20
 
-struct event {
-  char prev_comm[16];
-  int prev_pid;
-  char next_comm[16];
-  int next_pid;
-  __u32 cpu;
-};
+// start idle time of per-cpu
+ struct {
+   __uint(type, BPF_MAP_TYPE_ARRAY);
+   __uint(max_entries, MAX_MAP_ENTRIES);
+   __type(key, __u32);
+   __type(value, __u64);
+ } start_idle_time_map SEC(".maps");
 
-struct {
-  __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
-} events SEC(".maps");
+// idle duration time of per-cpu
+ struct {
+   __uint(type, BPF_MAP_TYPE_ARRAY);
+   __uint(max_entries, MAX_MAP_ENTRIES);
+   __type(key, __u32);
+   __type(value, __u64);
+ } idle_duration_time_map SEC(".maps");
 
 // Force emitting struct event into the ELF.
 const struct event *unused __attribute__((unused));
@@ -52,18 +40,38 @@ struct syscalls_sched_switch_args {
 
 SEC("tracepoint/sched/sched_switch")
 int sched_switch(struct syscalls_sched_switch_args *ctx) {
-  __u32 cpu = bpf_get_smp_processor_id();
-  //__u64 now = bpf_ktime_get_ns();
-  int prev_pid = ctx->prev_pid;
-  int next_pid = ctx->next_pid;
-  struct event event;
-  event.cpu = cpu;
-  event.prev_pid = prev_pid;
-  event.next_pid = next_pid;
-  char *prev_comm = (char *)(ctx->prev_comm);
-  bpf_probe_read_str(&event.prev_comm, sizeof(event.prev_comm), prev_comm);
-  char *next_comm = (char *)(ctx->next_comm);
-  bpf_probe_read_str(&event.next_comm, sizeof(event.next_comm), next_comm);
-  bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+      __u32 cpu = bpf_get_smp_processor_id();
+      __u64 now = bpf_ktime_get_ns();
+      int prev_pid = ctx->prev_pid;
+      int next_pid = ctx->next_pid;
+      if (next_pid == 0){
+            // update start_idle_time_map
+            __u64 *value = bpf_map_lookup_elem(&start_idle_time_map, &cpu);
+            if (value) {
+              bpf_map_update_elem(&start_idle_time_map, &cpu, &now,
+                                  BPF_EXIST);
+            } else {
+              bpf_map_update_elem(&start_idle_time_map, &cpu, &now,
+                                  BPF_NOEXIST);
+            }
+      }
+      if (prev_pid == 0){
+        // update idle_duration_time_map
+        __u64 *start_idle_time = bpf_map_lookup_elem(&start_idle_time_map, &cpu);
+        if (start_idle_time) {
+            __u64 duration_time = now - *start_idle_time;
+            __u64 *value = bpf_map_lookup_elem(&idle_duration_time_map, &cpu);
+            if (value) {
+                  duration_time = duration_time + *value;
+                  bpf_map_update_elem(&idle_duration_time_map, &cpu, &duration_time,
+                                      BPF_EXIST);
+                } else {
+                  bpf_map_update_elem(&idle_duration_time_map, &cpu, &duration_time,
+                                      BPF_NOEXIST);
+                }
+        }
+      }
+
+
   return 0;
 }
